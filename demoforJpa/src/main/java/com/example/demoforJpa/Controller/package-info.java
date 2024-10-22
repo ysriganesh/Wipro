9991f870d2ee01c1b6da622db7805d6c5465c935
Y.SRI GANESH REDDY <81284343+ysriganesh@users.noreply.github.com>
2024-10-22 23:04:33 +0530

    Add files via upload

---
+++ demoforJpa/src/main/java/com/example/demoforJpa/Controller/package-info.java
@@ -0,0 +1,1 @@
+package com.example.demoforJpa.Controller;
---
+++ demoforJpa/src/main/java/com/example/demoforJpa/DemoforJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoforJpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoforJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoforJpaApplication.class, args);
+	}
+
+}
