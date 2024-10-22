b4c8e65f152034ef75f87baa680b7734673c644d
Y.SRI GANESH REDDY <81284343+ysriganesh@users.noreply.github.com>
2024-10-22 23:13:20 +0530

    Delete demoforJpa directory

--- demoforJpa/src/main/java/com/example/demoforJpa/Controller/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.example.demoforJpa.Controller;
--- demoforJpa/src/main/java/com/example/demoforJpa/repo/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demoforJpa.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demoforJpa.entity.Student; // Adjust the package path accordingly
-
-// Student class that we are passing and the our primaray key is int type so Integer
-public interface StudentRepository extends JpaRepository<Student , Integer> {
-
-}
